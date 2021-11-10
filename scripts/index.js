8943660e66e928fddc2793055a386e39faeaf89a
Nataliesmyth <nataliesmyth11@gmail.com>
2021-11-10 17:20:40 -0500

    board toggle button

--- scripts/index.js
+++ scripts/index.js
@@ -75,3 +75,11 @@ console.log(teamBtn.value)
 // }
 
 
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
