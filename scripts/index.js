00e9fa764e6fdd3b5faacb8b6c7d77d766d68b3f
Nataliesmyth <nataliesmyth11@gmail.com>
2021-12-07 00:20:11 -0500

    search bar

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,44 @@
 console.log('sanity check')
 
-$('#navbarDropdown').on('show.bs.dropdown', function () {
-    // do something…
-    console.log('hello')
-  })
-
-$('#home-link').on('click', function() {
-  $('#main-index').hide()
-})
+  // search-box open close js code
+  let navbar = document.querySelector(".navbar");
+  let searchBox = document.querySelector(".search-box .bx-search");
+  // let searchBoxCancel = document.querySelector(".search-box .bx-x");
+  
+  searchBox.addEventListener("click", ()=>{
+    navbar.classList.toggle("showInput");
+    if(navbar.classList.contains("showInput")){
+      searchBox.classList.replace("bx-search" ,"bx-x");
+    }else {
+      searchBox.classList.replace("bx-x" ,"bx-search");
+    }
+  });
+  
+  // sidebar open close js code
+  let navLinks = document.querySelector(".nav-links");
+  let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+  let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+  menuOpenBtn.onclick = function() {
+  navLinks.style.left = "0";
+  }
+  menuCloseBtn.onclick = function() {
+  navLinks.style.left = "-100%";
+  }
+  
+  
+  // sidebar submenu open close js code
+  let htmlcssArrow = document.querySelector(".about-arrow");
+  htmlcssArrow.onclick = function() {
+   navLinks.classList.toggle("show1");
+  }
+  let moreArrow = document.querySelector(".more-arrow");
+  moreArrow.onclick = function() {
+   navLinks.classList.toggle("show2");
+  }
+  let jsArrow = document.querySelector(".services-arrow");
+  jsArrow.onclick = function() {
+   navLinks.classList.toggle("show3");
+  }
 
 // The rootElement is the element we are appending all of our content to
 const rootElement = document.getElementById('rootElement');
